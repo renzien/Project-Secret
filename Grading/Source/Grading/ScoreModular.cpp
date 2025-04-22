221b12a6efc79434900210fbcabaa7068e1d43eb
Alif Rizki Ramdhana <alifrizki1011@gmail.com>
2025-04-22 21:34:33 +0700

    initial commit

---
+++ Grading/Source/Grading/ScoreModular.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ScoreModular.h"
+
+// Sets default values
+AScoreModular::AScoreModular()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AScoreModular::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AScoreModular::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
